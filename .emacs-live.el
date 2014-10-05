eb0c19847b661fb2e69e4827429207d048156a2a
Luke Thomas <luk3thomas@gmail.com>
2014-10-04 22:29:57 -0500

    add elpa

--- .emacs-live.el
+++ .emacs-live.el
@@ -1,2 +1,8 @@
 (live-add-packs '(~/.live-packs/lthomas-pack))
 (live-add-packs '(~/.live-packs/lthomas-pack/lib/evilpack))
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  )
